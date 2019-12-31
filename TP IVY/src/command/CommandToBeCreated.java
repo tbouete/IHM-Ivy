ef14068806d1ddc3b200861fe22654c5cd06f720
Unknown <guilhem31.susa@hotmail.com>
2019-12-31 15:59:47 +0100

    Avancée code, E0,E1, bonne partie E2

--- TP IVY/src/command/CommandToBeCreated.java
+++ TP IVY/src/command/CommandToBeCreated.java
@@ -10,8 +10,8 @@ public class CommandToBeCreated {
 	private Ivy ivy;
 	private AvailableActions action;
 	private AvailableShapes shape;
-	private int posX;
-	private int posY;
+	private Integer posX;
+	private Integer posY;
 	private AvailableColors color;
 	
 	public CommandToBeCreated(Ivy ivy) {
@@ -55,6 +55,26 @@ public class CommandToBeCreated {
 	}
 	public void setColor(AvailableColors color) {
 		this.color = color;
+	}
+
+	public boolean isComplete() {
+		switch(action) {
+			case Creer:
+				if(shape != null 
+					&& posX != null
+					&& posY != null
+					&& color != null) {
+					return true;
+				}
+				break;
+			case Deplacer:
+				break;
+			case Supprimer:
+				break;
+			default:
+				return false;
+		}
+		return false;
 	}
 	
 	
